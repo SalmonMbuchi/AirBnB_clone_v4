4fc185e9dbd8e0b54283fcca236826089de0cb4b
SalmonMbuchi <salmonmbuchi@gmail.com>
2022-11-27 15:27:59 +0300

    upload new function

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -23,11 +23,30 @@ $(document).ready(function() {
     url: url + ':5001/api/v1/places_search/',
     data: {},
     dataType: 'json',
-    success: function (data) {
-      for (let i = 0; i < data.length; ++i) {
-        let html = '<article><div class="title_box"><h2>{{ place.name }}</h2><div class="price_by_night">${{ place.price_by_night }}</div></div><div class="information"><div class="max_guest">{{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div><div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>    <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{% endif %}</div> </div><div class="user"><b>Owner:</b> {{ place.user.first_name }} {{ place.user.last_name }}</div><div class="description">{{ place.description | safe }}</div></article>';
-        $('section.places').append(html);
-      }
-    }
+    success: fetchPlaces
   });
 });
+
+function fetchPlaces (data) {
+  $('section.places').empty();
+  $('section.places').append(data.map(place => {
+    return `<article>
+              <div class="title_box">
+                <h2>{{ place.name }}</h2>
+                <div class="price_by_night">{{ place.price_by_night }}</div>
+              </div>
+              <div class="information">
+                <div class="max_guest">{{ place.max_guest }} Guest{% if place.max_guest != 1 %}s{% endif %}</div>
+                <div class="number_rooms">{{ place.number_rooms }} Bedroom{% if place.number_rooms != 1 %}s{% endif %}</div>
+                <div class="number_bathrooms">{{ place.number_bathrooms }} Bathroom{% if place.number_bathrooms != 1 %}s{%
+                  endif %}</div>
+              </div>
+              <div class="user">
+                <b>Owner:</b> {{ place.user.first_name }} {{ place.user.last_name }}
+              </div>
+              <div class="description">
+                {{ place.description | safe }}
+              </div>
+            </article>`;
+  }));
+}
